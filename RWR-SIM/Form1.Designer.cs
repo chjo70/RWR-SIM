e49759607db9b17eb0b2cd75d700dce7615451ec
chjo70 <ss>
2019-03-07 21:36:37 +0900

    소스 추가

---
+++ RWR-SIM/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace RWR_SIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(358, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(19, 62);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(168, 131);
+            this.ButtonStart.TabIndex = 1;
+            this.ButtonStart.Text = "시작";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 500);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(799, 30);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(183, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 63);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.ButtonStart);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(643, 411);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 530);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ RWR-SIM/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+// 쓰레드 관련 함수
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace RWR_SIM
+{
+
+    enum REQ_MESSAGE 
+    {
+        REQ_INIT = 1,
+        REQ_SYSTEMVARIABLE
+    }
+
+    enum RES_MESSAGE
+    {
+        RES_INIT = 1,
+        RES_SYSTEMVARIABLE
+    }
+
+    public struct STRHeader
+    {
+        public uint opCode;
+        public uint uiDataLength;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal unsafe struct UNIHeader
+    {
+        [FieldOffset(0)]
+        STRHeader stHeader;
+
+        [FieldOffset(0)]
+        public fixed byte cData[8];
+    }
+
+    public struct STRMessage
+    {
+        public uint opCode;
+        public uint uiDataLength;
+        //byte[] cData=new int[100];
+    }
+
+  
+    public partial class Form1 : Form
+    {
+        bool m_bButtonStat=false;
+        CServer m_theServer;
+
+        public static Form1 _Form1;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            _Form1 = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DisplayStringInStatusBar();
+
+            m_theServer = new CServer();
+
+            Thread theThread = new Thread(new ThreadStart(m_theServer.ListenerThread));
+
+            theThread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m_bButtonStat)
+            {
+                ButtonStart.BackColor = Color.Cyan;
+                m_bButtonStat = false;
+            }
+
+            else
+            {
+                ButtonStart.BackColor = Color.White;
+                m_bButtonStat = true;
+            }
+        }
+
+        public void DisplayStringInStatusBar()
+        {
+            //
+            toolStripStatusLabel1.Text = String.Format( "연결 대기" );
+
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            
+            m_theServer.Close();
+        }
+
+    }
+
+
+    public class CServer
+    {
+        const int iTcpipPort = 1024;
+
+        public bool m_theListenerThread = true;
+
+        private TcpListener m_theClient;
+
+        public void ListenerThread()
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            m_theClient = new TcpListener(localAddr, iTcpipPort);
+            m_theClient.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            m_theClient.Start();
+
+            while (m_theListenerThread)
+            {
+                // _Form1.DisplayStringInStatusBar();
+                Console.WriteLine("AAA");
+
+                // 클라이언트의 연결 요청 확인
+                while (!m_theClient.Pending())
+                {
+                    Thread.Sleep(100);
+                    if( m_theListenerThread == false )
+                    {
+                        return;
+                    }
+                }
+
+                // 클라이언트와의 통신처리 스레드 시작
+                ConnectionHandler newConnection = new ConnectionHandler();
+                newConnection.m_theThreadListener = this.m_theClient;
+                Thread newThread = new Thread(new ThreadStart(newConnection.ClientHandler));
+                newThread.Start();
+
+            }
+        }
+
+        public void Close()
+        {
+            m_theListenerThread = false;
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    struct DataPacket
+    {
+        [MarshalAs(UnmanagedType.I2)]
+        public ushort opCode;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public ushort dataLength;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string Subject;
+
+        // Calling this method will return a byte array with the contents
+        // of the struct ready to be sent via the tcp socket.
+        public byte[] Serialize()
+        {
+            // allocate a byte array for the struct data
+            var buffer = new byte[Marshal.SizeOf(typeof(DataPacket))];
+
+            // Allocate a GCHandle and get the array pointer
+            var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var pBuffer = gch.AddrOfPinnedObject();
+
+            // copy data from struct to array and unpin the gc pointer
+            Marshal.StructureToPtr(this, pBuffer, false);
+            gch.Free();
+
+            return buffer;
+        }
+
+        // this method will deserialize a byte array into the struct.
+        public void Deserialize(ref byte[] data)
+        {
+            var gch = GCHandle.Alloc(data, GCHandleType.Pinned);
+            this = (DataPacket)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(DataPacket));
+            gch.Free();
+        }
+    }
+
+    public class ConnectionHandler
+    {
+        public TcpListener m_theThreadListener;
+        private DataPacket m_theRecvPacket;
+        private DataPacket m_theSendvPacket;
+        private NetworkStream m_theNS;
+        //public DataPacket m_thePacket = new DataPacket();
+
+        public ConnectionHandler()
+        {
+            m_theRecvPacket = new DataPacket();
+            m_theSendvPacket = new DataPacket();
+        }
+
+        public void ClientHandler()
+        {
+            TcpClient theClient = m_theThreadListener.AcceptTcpClient();
+            m_theNS = theClient.GetStream();
+
+            byte[] cData = new byte[20];
+
+            while (true)
+            {
+
+                try
+                {
+                    int ret = m_theNS.Read(cData, 0, Marshal.SizeOf(typeof(STRHeader)) );
+
+                    m_theRecvPacket.Deserialize(ref cData);
+
+                    ParsingPacket();
+
+                    if (ret == 0)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        //uniHeaderMessage.cData = cData;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    if (!theClient.Connected)
+                    {
+                        return;
+                    }
+                }
+            }
+            
+            
+        }
+
+        ~ConnectionHandler()
+        {
+
+        }
+
+        public void ParsingPacket()
+        {
+            byte[] buffer;
+
+            switch (m_theRecvPacket.opCode)
+            {
+
+                case (int) REQ_MESSAGE.REQ_INIT:
+                    m_theSendvPacket.opCode = (int) RES_MESSAGE.RES_INIT;
+                    m_theSendvPacket.dataLength = 0;
+                    buffer = m_theSendvPacket.Serialize();
+                    m_theNS.Write(buffer, 0, Marshal.SizeOf(typeof(STRHeader)));
+                    break;
+
+                default :
+                    break;
+            }
+        }
+    }
+
+
+
+
+
+
+}
